3384df31bc4fd44a2c6f4a632eef47f4d142aca0
Eugene Gudkov <31276495+xme1ez@users.noreply.github.com>
2019-01-07 19:57:33 +0200

    Update cocktail_sort.py

--- sorting_methods/cocktail_sort.py
+++ sorting_methods/cocktail_sort.py
@@ -5,22 +5,24 @@
 
 def cocktail_sort(array):
 
-	h = 0
-	t = 0
+	h = 0  # position from the beginning of the array
+	t = 0  # position from the end of the array
 	while h+t < len(array):
 
+		# moving highest number to the end of array
 		for j in range(h, len(array)-1-t):
 			if array[j] > array[j+1]:
 				tmp = array[j]
 				array[j] = array[j+1]
 				array[j+1] = tmp
-		t += 1
+		t += 1  # move end of array for consideration to position -1
 
+		# moving lowest number to the beginning.
 		for j in range(-(t+1), -((len(array)+1)-h), -1):
 			if array[j] > array[j+1]:
 				tmp = array[j]
 				array[j] = array[j+1]
 				array[j+1] = tmp
-		h += 1
+		h += 1  # move beginning of array for consideration to position -1
 
 	return array
