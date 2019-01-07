cbcacb1cd766c1917bcd6e4b5c8078353247cfbc
Eugene Gudkov <31276495+xme1ez@users.noreply.github.com>
2019-01-07 20:02:33 +0200

    Update bubble_sort.py

--- sorting_methods/bubble_sort.py
+++ sorting_methods/bubble_sort.py
@@ -5,6 +5,7 @@
 def bubble_sort(array):
 
 	for i in range(len(array)-1):
+		# have to move pointer on beginning element of array each iteration on position +1
 		for j in range(i, len(array)):
 			# on each step have to compare two numbers and if condition is met have to swap this numbers
 			if array[i] > array[j]:  # '>' - Sort Ascending; '<' - Sort Descending
