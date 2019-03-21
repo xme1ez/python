1ea925ca351275203da2b4f0dede06abc3a1534c
Eugene Gudkov <31276495+xme1ez@users.noreply.github.com>
2019-03-21 23:44:17 +0200

    Add files via upload

---
+++ EulerTasks/task1.py
@@ -0,0 +1,17 @@
+# determining the sum of all numbers less than MAX variable, multiples of
+# numbers at list.
+
+
+def determining(max, dividers):
+
+	sum = 0
+	for num in range(max):  # loop through numbers in searchable range
+		for elem in dividers:  # loop through dividers
+			if num % elem == 0:
+				sum += num
+				break
+
+	return sum
+
+
+print(determining(1000, [3, 5]))
